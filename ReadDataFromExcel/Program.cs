8493c3553f2d3eb8284d419201082005406ce878
Plangfromwow <jacobwantswow@gmail.com>
2024-05-25 12:40:19 -0400

    Added comments for clarity on what is going on

--- ReadDataFromExcel/Program.cs
+++ ReadDataFromExcel/Program.cs
@@ -18,49 +18,54 @@ Excel.Range excelRange = ws.UsedRange;
 int rowCount = excelRange.Rows.Count;
 int columnCount = excelRange.Columns.Count;
 
-//for (int i = 1; i <= rowCount; i++)
-//{
-//    for (int j = 1; j <= columnCount; j++)
-//    {
-//        if (excelRange.Cells[i, j] != null)
-//        {
-//            if(excelRange.Cells[i, j].Value2 != null)
-//            {
-//            Console.Write(excelRange.Cells[i,j].Value2.ToString()+" ");
-//            }
-//        }
-//    }
-//}
+//Initial loop over using the xlsApp but seems to be more clumsy than what I do later on.
+/*
+for (int i = 1; i <= rowCount; i++)
+{
+    for (int j = 1; j <= columnCount; j++)
+    {
+        if (excelRange.Cells[i, j] != null)
+        {
+            if(excelRange.Cells[i, j].Value2 != null)
+            {
+            Console.Write(excelRange.Cells[i,j].Value2.ToString()+" ");
+            }
+        }
+    }
+}
 
+*/
 
-//using (var context = new FinanceAppContext())
-//for (int i =0; i < rowCount - 1; i++)
-//{
-//    DateTime PostDateData = ws.Range["B" + (2 + i)].Value;
-//    var DescriptionData = ws.Range["C" + (2 + i)].Value;
-//    double AmountData = ws.Range["D" + (2 + i)].Value;
-//    var TypeData = ws.Range["E" + (2 + i)].Value;
-////    double AmountData = ws.Range["F" + (2 + i)].Value;
-//    //var MemoData = ws.Range["F" + (2 + i)].Value;
+// Reads from the excel document and writes the new object to the DB 
+/*
+using (var context = new FinanceAppContext())
+    for (int i = 0; i < rowCount - 1; i++)
+    {
+        DateTime PostDateData = ws.Range["B" + (2 + i)].Value;
+        var DescriptionData = ws.Range["C" + (2 + i)].Value;
+        double AmountData = ws.Range["D" + (2 + i)].Value;
+        var TypeData = ws.Range["E" + (2 + i)].Value;
+        //    double AmountData = ws.Range["F" + (2 + i)].Value;
+        //var MemoData = ws.Range["F" + (2 + i)].Value;
 
-//    var newFin = new FinanceInfo()
-//    {
-//        PostDate = PostDateData.ToShortDateString(),
-//        Description = DescriptionData,
-//        //Category = CategoryData,
-//        Type = TypeData,
-//        Amount = AmountData,
-//        //Memo = MemoData
-//        CardType = "DebitCard"
-//    };
+        var newFin = new FinanceInfo()
+        {
+            PostDate = PostDateData.ToShortDateString(),
+            Description = DescriptionData,
+            //Category = CategoryData,
+            Type = TypeData,
+            Amount = AmountData,
+            //Memo = MemoData
+            CardType = "DebitCard"
+        };
 
-//    context.FinanceInfos.Add(newFin);
-//    context.SaveChanges();
-//    Console.WriteLine(newFin + "was written to the DB");
-//}
+        context.FinanceInfos.Add(newFin);
+        context.SaveChanges();
+        Console.WriteLine(newFin + "was written to the DB");
+    }
+*/
 
-
-
+// Closes the open connections to the excel sheet. Is this necessary? Is there no way to do this once the using statement is done in the context window? Must be a better way.
 Marshal.ReleaseComObject(excelRange);
 Marshal.ReleaseComObject(ws);
 Marshal.ReleaseComObject(wb);
